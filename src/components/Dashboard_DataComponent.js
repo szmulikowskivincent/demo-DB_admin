757a12cb1f3eb839ff3260b529f00b92cb4d46da
szmulikowski vincent <szmulikowski.vincent.1971@hotmail.com>
2024-09-25 12:38:52 +0200

    $ git push --force-with-lease

--- src/components/Dashboard_DataComponent.js
+++ src/components/Dashboard_DataComponent.js
@@ -2,12 +2,19 @@ import React from "react";
 import "../css/Dashboard_Data.css";
 import UserTable from "./UserTable";
 import ContactListComponent from "./ContactListComponent";
+import avatar from "../assets/avatar1.png";
 
 const Dashboard_dataComponent = () => {
   return (
-    <div>
-      <h2>Connexion données utilisateurs</h2>
-      <p align="center">🟢 Vous êtes connecté!</p>
+    <div className="dashboard-container">
+      <div className="dashboard-header">
+        <br />
+        <img src={avatar} alt="Avatar" className="dashboard-avatar" />
+        <div>
+          <h2>Connexion données utilisateurs</h2>
+          <p align="center">🟢 Vous êtes connecté!</p>
+        </div>
+      </div>
       <UserTable />
       <ContactListComponent />
     </div>
